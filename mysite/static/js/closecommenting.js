50a9e0cc9d61be3514666d319c0d5551406a5e1f
Stéphani Vilayphiou <stephanie@stdin.fr>
2010-02-24 22:05:55 +0100

    rm border of arrow, toggle up and down arrow

--- mysite/static/js/closecommenting.js
+++ mysite/static/js/closecommenting.js
@@ -4,9 +4,11 @@ $(document).ready(function() {
         e.preventDefault();
         $('.comment-block', $(this).parents()[1]).css('display', 'block');
         $('#add-comment', $(this).parents()[1]).css('display', 'block');
+        $(this).text(' ⬆');
     }, function(e) {
         e.preventDefault();
         $('.comment-block', $(this).parents()[1]).css('display', 'none');
         $('#add-comment', $(this).parents()[1]).css('display', 'none');
+        $(this).text(' ⬇');
     });
 });
