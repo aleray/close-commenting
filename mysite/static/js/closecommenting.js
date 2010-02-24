773dd23c14029207beaa002971f59011433eaa01
Stéphani Vilayphiou <stephanie@stdin.fr>
2010-02-24 23:00:58 +0100

    relation of visibility between addcomment form and comment-block

--- mysite/static/js/closecommenting.js
+++ mysite/static/js/closecommenting.js
@@ -1,14 +1,30 @@
 
 $(document).ready(function() {
-    $('.collapse').toggle(function(e) {
-        e.preventDefault();
+    // If add-comment form, then comment-block is visible
+    if ($('#add-comment', $(this).parents()[1]).css('display') == 'block') {
         $('.comment-block', $(this).parents()[1]).css('display', 'block');
-        $('#add-comment', $(this).parents()[1]).css('display', 'block');
-        $(this).text(' ⬆');
+        $('.collapse-block').text('⬆');
+    }
+    // Show/Hide a comment-block
+    $('.collapse-block').click(function(e) {
+        e.preventDefault();
+        if ($('.comment-block', $(this).parents()[1]).css('display') == 'block') {
+            $('.comment-block', $(this).parents()[1]).css('display', 'none');
+            $(this).text('⬇');
+        } else {
+            $('.comment-block', $(this).parents()[1]).css('display', 'block');
+            $(this).text('⬆');
+        }
+    });
+
+    // Show/Hide single comment
+    $('.collapse-one').toggle(function(e) {
+        e.preventDefault();
+        $('.comment-text', $(this).parents()[1]).css('height', '0.8em');
+        $(this).text('⬇');
     }, function(e) {
         e.preventDefault();
-        $('.comment-block', $(this).parents()[1]).css('display', 'none');
-        $('#add-comment', $(this).parents()[1]).css('display', 'none');
-        $(this).text(' ⬇');
+        $('.comment-text', $(this).parents()[1]).css('height', 'auto');
+        $(this).text('⬆');
     });
 });
